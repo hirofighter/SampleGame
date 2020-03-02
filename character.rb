e0ed200cb2aff62ba847ca8c2e355ba235010d53
hiroki nakahashi <hirofighter@gmail.com>
2020-03-03 00:16:32 +0900

    add introduction method

--- character.rb
+++ character.rb
@@ -8,4 +8,17 @@ class Character
     @hp = param[:hp] #HP
   end
 
+  def introduction
+    puts <<~ EOS
+    ライフ：#{@hp}
+    体力：#{@physical}
+    攻撃力：#{@attack}
+    守備力：#{@guard}
+    EOS
+  end
+
 end
+
+hero = Character.new(physical: 100, attack: 50,guard: 30, hp: 20)
+
+hero.introduction
---
+++ hero.rb
@@ -0,0 +1,9 @@
+class Hero < Character
+
+  # def test
+  #   hero = Character.new(physical: 100, attack: 50,guard: 30, hp: 20)
+
+  #   hero.in
+  # end
+
+end

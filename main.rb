b3768f4547ee3c612eef33918a8d0b66da5218fb
hiroki nakahashi <hirofighter@gmail.com>
2020-03-04 23:54:01 +0900

    勇者クラスとモンスタークラス実装

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
-class Brave
-  attr_accessor :name, :attack, :guard, :hp
+class Character
+  attr_reader :name, :attack, :guard, :hp
+  attr_accessor :hp
 
   def initialize(**param)
     @name = param[:name] #名前
@@ -10,18 +11,40 @@ class Brave
 
 end
 
-# 勇者クラスをインスタンス化
-brave = Brave.new
+class Brave < Character
 
-# パラメータ
-brave.name = "マイケル"
-brave.hp = 500
-brave.attack = 100
-brave.guard = 70
+  def introduce
+    puts "私は勇者"
+  end
+end
 
+
+class Monster < Character
+  def introduce
+    puts "俺様はモンスター"
+  end
+end
+
+
+# キャラクタークラスをインスタンス化
+brave = Brave.new(name: "マイケル", hp: 500, attack: 100, guard: 70)
+monster = Monster.new(name: "バラモス", hp: 700, attack: 80, guard: 30)
+
+
+brave.introduce
 puts <<~TEXT
-名前：#{brave.name}
+#{brave.name}
 HP：#{brave.hp}
 攻撃力：#{brave.attack}
 守備力：#{brave.guard}
+----------------
+TEXT
+
+monster.introduce
+puts <<~TEXT
+名前：#{monster.name}
+HP：#{monster.hp}
+攻撃力：#{monster.attack}
+守備力：#{monster.guard}
+----------------
 TEXT
